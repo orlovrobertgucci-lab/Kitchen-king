c86c1d01cf8eac2a07ade9e318dd139f43cce5da
orlovrobertgucci-lab <orlov.robert.gucci@gmail.com>
2025-09-30 00:46:33 +0300

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,15 @@
+const portfolioItems = [
+  {
+    id: 1,
+    title: "Классика с островом",
+    description: "Бежевая кухня с золотой фурнитурой для 4-комнатной квартиры в Чебоксарах",
+    image: "https://i.ibb.co/YfGx8Kz/classic-kitchen-cheboksary.jpg"
+  },
+  {
+    id: 2,
+    title: "Скандинавский стиль",
+    description: "Светлая кухня из массива сосны для загородного дома под Чебоксарами",
+    image: "https://i.ibb.co/7yVxR2m/scandinavian-kitchen.jpg"
+  },
+  // ... и так далее
+];
